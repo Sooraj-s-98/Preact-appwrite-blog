e597b872135768ea25c5bb1c511677bdd988dfd0
sooraj <sooraj@mozilor.com>
2022-10-22 18:58:29 +0530

    login and signup ui change

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,53 +1,59 @@
-import api  from '../api/api'
-import { v4 as uuid } from 'uuid';
+import api from "../api/api";
+import { v4 as uuid } from "uuid";
 import { signal } from "@preact/signals";
-import { route } from 'preact-router';
-import { Link } from 'preact-router/match';
+import { route } from "preact-router";
+import { Link } from "preact-router/match";
 
-const email = signal("")
-const password = signal("")
+const email = signal("");
+const password = signal("");
 
 const setEmail = (e) => {
-    email.value = e.target.value
-}
+  email.value = e.target.value;
+};
 
 const setPassword = (e) => {
-    password.value = e.target.value
-}
+  password.value = e.target.value;
+};
 
 export default function Login() {
 
-    const login =async (e) => {
-        e.preventDefault()
-        await api.createSession(email.value, password.value);
-        const user = await api.getAccount();
-        route('/', true)
-    }
+  const login = async (e) => {
+    e.preventDefault();
+    await api.createSession(email.value, password.value);
+    const user = await api.getAccount();
+    route("/", true);
+  };
 
-    return (
-        <div class='cont'>
-            <div class="login">
-                <div class='login-form'>Login</div>
-                <div class='fields'>
-                    <p>Email</p>
-                    <input 
-                        type='email' 
-                        placeholder='Enter your email' required 
-                        onInput={setEmail}
-                    />
-                </div>
-                <div class='fields'>
-                    <p>Password</p>
-                    <input 
-                        placeholder='Enter your password' 
-                        required 
-                        type='password'
-                        onInput={setPassword}
-                    />
-                </div>
-                <button class='signup-btn' onClick={login}>Login</button>
-                <Link class='links' href='/signup'><p class='have-acc'>Create an Account?</p></Link>
-            </div>
+  return (
+    <div>
+      <div class="circle" />
+      <form class="form" onSubmit={login}>
+        <h2 class="form__title">Login</h2>
+        <div class="form__container">
+          <div class="form__group">
+            <input
+              type="email"
+              class="form__input"
+              placeholder="Enter your email"
+              required
+              onInput={setEmail}
+            />
+          </div>
+          <div class="form__group">
+            <input
+              class="form__input"
+              placeholder="Enter your password"
+              required
+              type="password"
+              onInput={setPassword}
+            />
+          </div>
+          <input type="submit" class="form__submit" value="LOG IN" />
+          <Link class="links" href="/signup">
+            <p class="have-acc">Create an Account?</p>
+          </Link>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,68 +1,72 @@
-import api from '../api/api'
-import { v4 as uuid } from 'uuid';
+import api from "../api/api";
 import { signal } from "@preact/signals";
-import { route } from 'preact-router';
-import { Link } from 'preact-router/match';
+import { Link } from "preact-router/match";
 
-const name = signal("")
-const email = signal("")
-const password = signal("")
+const name = signal("");
+const email = signal("");
+const password = signal("");
 
 const setName = (e) => {
-    name.value = e.target.value
-}
+  name.value = e.target.value;
+};
 
 const setEmail = (e) => {
-    email.value = e.target.value
-}
+  email.value = e.target.value;
+};
 
 const setPassword = (e) => {
-    password.value = e.target.value
-}
+  password.value = e.target.value;
+};
 
 export default function Signup() {
+  const signUp = async () => {
+    console.log("signUp");
+    const user = await api.createAccount(
+      email.value,
+      password.value,
+      name.value
+    );
+    console.log("user", user);
+    await api.createSession(email.value, password.value);
+  };
 
-    const signUp =async () => {
-        console.log("signUp")
-        const user = await api.createAccount(email.value, password.value, name.value);
-        console.log("user", user)
-        await api.createSession(email.value, password.value);
-    }
-
-    return (
-        <div class='cont'>
-            <div class="signup">
-                <div class='signup-form'>SignUp</div>
-                <div class='fields'>
-                    <p>User Name</p>
-                    <input 
-                        required 
-                        type='text' 
-                        placeholder='Enter your name'
-                        onInput={setName}
-                    />
-                </div>
-                <div class='fields'>
-                    <p>Email</p>
-                    <input 
-                        required  
-                        type='email'
-                        placeholder='Enter your email'
-                        onInput={setEmail}
-                    />
-                </div>
-                <div class='fields'>
-                    <p>Password</p>
-                    <input 
-                        required 
-                        placeholder='Enter your password'
-                        type='password'
-                        onInput={setPassword}
-                    />
-                </div>
-                <button class='signup-btn' onClick={()=>signUp()}>SignUp</button>
-                <Link href='/' class='links'>Have an Account!</Link>
-            </div>
+  return (
+    <div>
+      <div class="circle" />
+      <form class="form" onSubmit={Signup}>
+        <h2 class="form__title">SignUp</h2>
+        <div class="form__container">
+          <div class="form__group">
+            <input
+              required
+              class="form__input"
+              placeholder="Enter your name"
+              onInput={setName}
+            />
+          </div>
+          <div class="form__group">
+            <input
+              required
+              class="form__input"
+              placeholder="Enter your email"
+              onInput={setEmail}
+            />
+          </div>
+          <div class="form__group">
+            <input
+              class="form__input"
+              required
+              placeholder="Enter your password"
+              type="password"
+              onInput={setPassword}
+            />
+          </div>
+          <input type="submit" class="form__submit" value="SignUp" />
+          <Link href="/login" class="links">
+            Have an Account!
+          </Link>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
